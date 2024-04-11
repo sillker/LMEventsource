ef843c0aab798d521ee11992b66d154b6df56bbb
panhuifeng <panhuifeng@hqwx.com>
2024-04-11 10:37:21 +0800

    data 转 string 为nil处理

--- LDEventSource/LDEventSource.m
+++ LDEventSource/LDEventSource.m
@@ -152,17 +152,111 @@ didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSe
     }
 }
 
+//替换非utf8字符
+//注意：如果是三字节utf-8，第二字节错误，则先替换第一字节内容(认为此字节误码为三字节utf8的头)，然后判断剩下的两个字节是否非法；
+- (NSData *)replaceNoUtf8:(NSData *)data
+{
+    //保存结果
+    NSMutableData *resData = [[NSMutableData alloc] initWithCapacity:data.length];
+
+    NSData *replacement = [@"�" dataUsingEncoding:NSUTF8StringEncoding];
+
+    uint64_t index = 0;
+    const uint8_t *bytes = data.bytes;
+
+    long dataLength = (long) data.length;
+
+    while (index < dataLength) {
+        uint8_t len = 0;
+        uint8_t firstChar = bytes[index];
+
+            // 1个字节
+        if ((firstChar & 0x80) == 0 && (firstChar == 0x09 || firstChar == 0x0A || firstChar == 0x0D || (0x20 <= firstChar && firstChar <= 0x7E))) {
+            len = 1;
+        }
+            // 2字节
+        else if ((firstChar & 0xE0) == 0xC0 && (0xC2 <= firstChar && firstChar <= 0xDF)) {
+            if (index + 1 < dataLength) {
+                uint8_t secondChar = bytes[index + 1];
+                if (0x80 <= secondChar && secondChar <= 0xBF) {
+                    len = 2;
+                }
+            }
+        }
+            // 3字节
+        else if ((firstChar & 0xF0) == 0xE0) {
+            if (index + 2 < dataLength) {
+                uint8_t secondChar = bytes[index + 1];
+                uint8_t thirdChar = bytes[index + 2];
+                
+                if (firstChar == 0xE0 && (0xA0 <= secondChar && secondChar <= 0xBF) && (0x80 <= thirdChar && thirdChar <= 0xBF)) {
+                    len = 3;
+                } else if (((0xE1 <= firstChar && firstChar <= 0xEC) || firstChar == 0xEE || firstChar == 0xEF) && (0x80 <= secondChar && secondChar <= 0xBF) && (0x80 <= thirdChar && thirdChar <= 0xBF)) {
+                    len = 3;
+                } else if (firstChar == 0xED && (0x80 <= secondChar && secondChar <= 0x9F) && (0x80 <= thirdChar && thirdChar <= 0xBF)) {
+                    len = 3;
+                }
+            }
+        }
+            // 4字节
+        else if ((firstChar & 0xF8) == 0xF0) {
+            if (index + 3 < dataLength) {
+                uint8_t secondChar = bytes[index + 1];
+                uint8_t thirdChar = bytes[index + 2];
+                uint8_t fourthChar = bytes[index + 3];
+                
+                if (firstChar == 0xF0) {
+                    if ((0x90 <= secondChar & secondChar <= 0xBF) && (0x80 <= thirdChar && thirdChar <= 0xBF) && (0x80 <= fourthChar && fourthChar <= 0xBF)) {
+                        len = 4;
+                    }
+                } else if ((0xF1 <= firstChar && firstChar <= 0xF3)) {
+                    if ((0x80 <= secondChar && secondChar <= 0xBF) && (0x80 <= thirdChar && thirdChar <= 0xBF) && (0x80 <= fourthChar && fourthChar <= 0xBF)) {
+                        len = 4;
+                    }
+                } else if (firstChar == 0xF3) {
+                    if ((0x80 <= secondChar && secondChar <= 0x8F) && (0x80 <= thirdChar && thirdChar <= 0xBF) && (0x80 <= fourthChar && fourthChar <= 0xBF)) {
+                        len = 4;
+                    }
+                }
+            }
+        }
+            // 5个字节
+        else if ((firstChar & 0xFC) == 0xF8) {
+            len = 0;
+        }
+            // 6个字节
+        else if ((firstChar & 0xFE) == 0xFC) {
+            len = 0;
+        }
+
+        if (len == 0) {
+            index++;
+            [resData appendData:replacement];
+        } else {
+            [resData appendBytes:bytes + index length:len];
+            index += len;
+        }
+    }
+
+    return resData;
+}
+
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
 {
     NSString *eventString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    if (self.receiveTempString.length) {
+    
+    if (!eventString) {
+        data = [self replaceNoUtf8:data];
+        eventString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    if (self.receiveTempString.length && eventString) {
 //        char *cStr = [self.receiveTempString cStringUsingEncoding:NSUTF8StringEncoding];
 //        printf("+++收到数据unMatchString: %s \n", cStr);
         eventString = [self.receiveTempString stringByAppendingString:eventString];
         self.receiveTempString = nil;
     }
-//    char *cStr = [eventString cStringUsingEncoding:NSUTF8StringEncoding];
-//    printf("+++收到数据: %s \n", cStr);
+    char *cStr = [eventString cStringUsingEncoding:NSUTF8StringEncoding];
+    printf("+++收到数据: %s \n", cStr);
     if ([eventString containsString:@"event:"]) {
         NSString *pattern = @"^(event:).*(\\n\\n)";
         NSError *error = nil;
